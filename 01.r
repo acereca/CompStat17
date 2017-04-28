a4fb900a8f4995dfac006ac41093d2ea4eb2e089
acereca <tigerauge96@live.de>
2017-04-28 10:43:31 +0200

    update to info.d

--- 01.r
+++ 01.r
@@ -22,7 +22,7 @@ paste('mean:',mean(mon))
 paste('std. dev:',sd(mon))
 
 pdf('01-1.pdf')
-hist(mat)
+hist(mat, breaks= 0:13)
 
 survey_hist = hist(mat)
 pdf('01-2.pdf')
@@ -31,4 +31,18 @@ barplot(survey_hist$counts/sum(survey_hist$counts),
     xlab='month')
 
 pdf('01-3.pdf')
-hist(mon)
+hist(mon, breaks=0:30)
+
+
+# problem 2: birthday paradow
+
+ndata = 2:50
+exactdata = 1-(365-ndata+1)**(ndata-1)/(365**(ndata-1))
+assumdata = 1-exp(-ndata*(ndata-1)/365/2)
+assumdata2 = 1-exp(-ndata**2/365/2)
+
+pdf('01-4.pdf')
+plot(ndata,exactdata, pch='.', cex=3)
+points(ndata,assumdata, pch='.', col=2, cex=3)
+points(ndata,assumdata2, pch='.', col=3, cex=3)
+legend(1,1,c("exact","assumed exp.","assumed exp. 2"),col=c(1,2,3),pch=c('-','-','-'))
--- info.md
+++ info.md
@@ -1,2 +1,4 @@
 # Info to Computational Statistics SoSe17
  - 6 marked sheets
+ - 1 every 2 weeks
+ - 1 week per sheet
